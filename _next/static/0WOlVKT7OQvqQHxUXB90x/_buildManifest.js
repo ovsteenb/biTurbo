780fe581046db5ff58d9a36c8548383dc4a19e4a
ltfysl <33989040+ltfysl@users.noreply.github.com>
2026-06-10 20:56:28 +0200

    Updates

---
+++ _next/static/0WOlVKT7OQvqQHxUXB90x/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-7b6843aa0302a87e.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
